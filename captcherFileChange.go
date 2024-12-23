d9fecad57912be7c42651bb97dd76a2516703fa9
kosalaMaduwantha <kosalamaduwantha0@gmail.com>
2024-12-23 07:46:58 +0530

    add launch configuration for debugging and update file change detection logic

--- captcherFileChange.go
+++ captcherFileChange.go
@@ -5,6 +5,7 @@ import (
 	"cdc-file-processor/packages/hashOps"
 	"cdc-file-processor/packages/jsonOps"
 	"time"
+
 	"github.com/sirupsen/logrus"
 )
 
@@ -17,28 +18,37 @@ func isFileChanged(inputFilePath string, configFilePath string) bool {
 
 func main() {
 	logger := logrus.New()
-    logger.SetLevel(logrus.InfoLevel)
-    logger.SetFormatter(&logrus.JSONFormatter{})
-	
-    const inputFilePath string = "files/input.txt"
-    const configFilePath string = "files/hashStore.json"
-    for {
-        if isFileChanged(inputFilePath, configFilePath) {
-            logger.Info("File is changed")
-            hashDataStore := jsonOps.JsonDeserializeFileHashData(
+	logger.SetLevel(logrus.InfoLevel)
+	logger.SetFormatter(&logrus.JSONFormatter{})
+
+	const inputFilePath string = "files/input.txt"
+	const configFilePath string = "files/hashStore.json"
+	for {
+		if isFileChanged(inputFilePath, configFilePath) {
+			logger.Info("File is changed")
+
+			hashDataStore := jsonOps.JsonDeserializeFileHashData(
 				fileOps.ReadFileContent(configFilePath))
-            lineCount := hashDataStore.LineCount
-            if lineCount == 0 {lineCount = 1}
-            fileContent, currentLineNo := fileOps.ReadFileContentLineByLine(
+			lineCount := hashDataStore.LineCount
+			if lineCount == 0 {
+				lineCount = 1
+			}
+
+			fileContent, currentLineNo := fileOps.ReadFileContentLineByLine(
 				inputFilePath, lineCount)
-            newFileHash := hashOps.HashFile(inputFilePath)
-            jsonData := jsonOps.JsonSerializeFileHashData(
+
+			newFileHash := hashOps.HashFile(inputFilePath)
+			jsonData := jsonOps.JsonSerializeFileHashData(
 				newFileHash, configFilePath, currentLineNo)
-            fileOps.FileWriter(configFilePath, jsonData)
-            logger.WithFields(logrus.Fields{"content: ": fileContent,}).Info("File content is")
-        } else {
-            logger.Info("File is not changed")
-        }
-        time.Sleep(5 * time.Second)
-    }
+			fileOps.FileWriter(configFilePath, jsonData)
+
+			logger.WithFields(
+				logrus.Fields{
+					"content: ": fileContent,
+				}).Info("File content is")
+		} else {
+			logger.Info("File is not changed")
+		}
+		time.Sleep(5 * time.Second)
+	}
 }
